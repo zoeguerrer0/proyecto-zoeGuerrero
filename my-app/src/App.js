0ad0fd8cf87a946c342aa30bc33da98ac4510d08
zoeGuerrero <zoeguerreroepet20@gmail.com>
2024-06-25 21:53:00 -0300

    commit 1

---
+++ my-app/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {text} from  'react-native-gesture-handler';//importamos link para utilizar la librería react-navigation
+const App =()=>{
+    return <div>
+        hello world
+    </div>
+};
+export default App;
